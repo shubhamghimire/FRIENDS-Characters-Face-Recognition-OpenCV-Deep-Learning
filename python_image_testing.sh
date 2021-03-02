eb12fa36f8de3632e84eb8554106fb63706bc222
shubhamghimire <shubhamghimire1996@gmail.com>
2021-03-02 15:37:31 +0900

    all files are added

---
+++ python_image_testing.sh
@@ -0,0 +1,8 @@
+<<<<<<< HEAD
+for i in {1..16}; do 
+     python recognize_faces_image.py --encodings encodings.pickle --image examples/ex-"$i".jpg --output output_result_image/ex-"$i"-output.jpg
+=======
+for i in {1..16}; do 
+     python recognize_faces_image.py --encodings encodings.pickle --image examples/ex-"$i".jpg --output output_result_image/ex-"$i"-output.jpg
+>>>>>>> 380547013f40779088488b9dead228598105c26a
+done
